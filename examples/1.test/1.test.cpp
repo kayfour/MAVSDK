08ab2e99a97afe5d80c37a922e2610c330773f66
Junhee Cho <junhee3085@gmail.com>
2020-12-01 10:28:41 +0900

    landing

--- examples/1.test/1.test.cpp
+++ examples/1.test/1.test.cpp
@@ -18,14 +18,14 @@ int main(int argc, char** argv)
 
     if (connection_result != ConnectionResult::Success) {return 1;}
 
-    mavsdk.subscribe_on_new_system([&mavsdk, &discovered_system]() {
+    mavsdk.subscribe_on_new_system([&mavsdk, &discovered_system]() {    // 콜백함수 with 익명함수
         const auto system = mavsdk.systems().at(0);
-        if (system->is_connected()) {discovered_system = true;}
+        if (system->is_connected()) {discovered_system = true;} // 시스템 연결 확인
     });
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::this_thread::sleep_for(std::chrono::seconds(2));   // 콜백함수에 의한 시스템 연결 대기
 
-    if (!discovered_system) {return 1;}
+    if (!discovered_system) {return 1;} // 시스템 디텍트 실패시 리턴 1
     const auto system = mavsdk.systems().at(0);
 
     system->register_component_discovered_callback(
@@ -34,23 +34,23 @@ int main(int argc, char** argv)
     //==============================================================================================
     // 2.regist_telemetry
     //==============================================================================================
-    auto telemetry = std::make_shared<Telemetry>(system);
+    auto telemetry = std::make_shared<Telemetry>(system);   // 원격 측정
 
-    const Telemetry::Result set_rate_result = telemetry -> set_rate_position(1.0);
+    const Telemetry::Result set_rate_result = telemetry -> set_rate_position(1.0);  // 1초마다 수신
     if(set_rate_result != Telemetry::Result::Success){return 1;}
-    telemetry -> subscribe_position([](Telemetry::Position position){
-            std::cout << "Altitude: " << position.relative_altitude_m << "m" << std::endl;
+    telemetry -> subscribe_position([](Telemetry::Position position){   // 고도를 모니터링하기 위한 콜백함수, 익명함수
+            std::cout << "Altitude: " << position.relative_altitude_m << "m" << std::endl;  
     });
     while (telemetry -> health_all_ok() != true){
         std::cout << "Vehicle is getting ready to arm" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     //==============================================================================================
-    // 3.arm -> pxh> commander arm
+    // 3.arm
     //==============================================================================================
     auto action = std::make_shared<Action>(system);
     std:: cout << "Arming..." << std::endl;
-    const Action::Result arm_result = action -> arm();
+    const Action::Result arm_result = action -> arm();  // 암
 
     if(arm_result != Action::Result::Success){
         std::cout << "Arming failed: " << arm_result <<std::endl;
@@ -60,11 +60,28 @@ int main(int argc, char** argv)
     // 4.takeoff
     //==============================================================================================
     std::cout << "Taking off..." << std::endl;
-    const Action::Result takeoff_result = action -> takeoff();
+    const Action::Result takeoff_result = action -> takeoff();  // 이륙
     if(takeoff_result != Action::Result::Success){
         std::cout << "Take off failed: " << takeoff_result << std::endl;
         return 1;
     }
+    //==============================================================================================
+    // 5.land
+    //==============================================================================================
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "Landing..." << std::endl;
+    const Action::Result land_result = action -> land();    // 랜딩
+
+    if(land_result != Action::Result::Success){
+        std::cout << "land failed: " << takeoff_result << std::endl;
+        return 1;
+    }
+    while(telemetry -> in_air()){   // 공중에 있는지 체크
+        std::this_thread::sleep_for(std::chrono::seconds(1));   // 주기 1초
+    }
+    std::cout << "Landed" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << "Finished..." << std::endl;
 
     return 0;
 }
